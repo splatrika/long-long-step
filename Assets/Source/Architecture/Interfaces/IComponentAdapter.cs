50d0ffb8d066c73d1c7146b81cf6a3163c39f091
daniilbelov <tseflutty@yandex.ru>
2022-10-29 11:18:58 +0300

    Fix component adapter interface: it wasn't an interface

--- Assets/Source/Architecture/Interfaces/IComponentAdapter.cs
+++ Assets/Source/Architecture/Interfaces/IComponentAdapter.cs
@@ -1,21 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Splatrika.LongLongStep.Architecture
 {
-    public class IComponentAdapter : MonoBehaviour
+    public interface IComponentAdapter
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
     }
 }
