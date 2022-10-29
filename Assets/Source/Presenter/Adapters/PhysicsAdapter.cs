9792439dfe3b43f6da76079a558c30faa5989ec4
daniilbelov <tseflutty@yandex.ru>
2022-10-29 11:36:23 +0300

    Add physics adapter

---
+++ Assets/Source/Presenter/Adapters/PhysicsAdapter.cs
@@ -0,0 +1,19 @@
+using Splatrika.LongLongStep.Architecture;
+using Splatrika.LongLongStep.Model;
+using UnityEngine;
+
+namespace Splatrika.LongLongStep.Presenter
+{
+    public class PhysicsAdapter
+        : MonoBehaviour, IPhysicsService, IComponentAdapter
+    {
+        public const string GroundLayer = "Ground";
+
+
+        public bool HasGround(Vector3 point)
+        {
+            var mask = LayerMask.GetMask(GroundLayer);
+            return Physics.Raycast(new Ray(point, Vector3.down), 0.2f, mask);
+        }
+    }
+}
