09b6af194ed32e7686fffefec09525303687ce25
daniilbelov <tseflutty@yandex.ru>
2022-10-31 15:46:02 +0300

    Add goal ground model

---
+++ Assets/Source/Model/Grounds/GoalGround.cs
@@ -0,0 +1,10 @@
+namespace Splatrika.LongLongStep.Model
+{
+    public class GoalGround : StaticGround, IGoalGround
+    {
+        public GoalGround(StaticGroundConfiguration configuration)
+            : base(configuration)
+        {
+        }
+    }
+}
