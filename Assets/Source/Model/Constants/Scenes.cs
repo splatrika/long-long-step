6f8de67f9216a5e1ed3288b2bb8097eaf787bcad
daniilbelov <tseflutty@yandex.ru>
2022-11-01 17:24:25 +0300

    Add scenes constants

---
+++ Assets/Source/Model/Constants/Scenes.cs
@@ -0,0 +1,8 @@
+namespace Splatrika.LongLongStep.Model
+{
+    public static class Scenes
+    {
+        public const string StartScreen = "StartScreen";
+        public const string LevelSelect = "LevelSelect";
+    }
+}
